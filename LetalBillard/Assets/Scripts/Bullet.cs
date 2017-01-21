46786ff8a4787d0a06ad30cd6032487112da2b69
DESKTOP-AHLI5F2\Maxence <m.beaumont@student.isartdigital.com>
2017-01-21 01:46:11 +0100

    Bullet.cs

---
+++ LetalBillard/Assets/Scripts/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField]
+    private const int SPEED = 10;
+
+    private bool isMoving = false;
+    private Rigidbody2D rgbg2D;
+    [SerializeField]
+    private int bounceCount = 0;
+    private Transform deathPos;
+    private Vector3 velocity;
+    public GameObject dieAnim;
+	
+    
+    public void initialize(float pAngle)
+    {
+        float lVelocityX = Mathf.Cos(pAngle * Mathf.Deg2Rad) * SPEED;
+        float lVelocityY = Mathf.Sin(pAngle * Mathf.Deg2Rad) * SPEED;
+        velocity = new Vector3(lVelocityX, lVelocityY);
+    }
+    
+    // Use this for initialization
+	void Start () {
+        rgbg2D = GetComponent<Rigidbody2D>();
+        isMoving = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isMoving)
+        {
+            rgbg2D.velocity = velocity;
+            isMoving = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bounceCount++;
+        if (bounceCount == 2)
+        {
+            deathPos = transform;
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("y");
+        Instantiate(dieAnim, new Vector3(deathPos.position.x, deathPos.position.y, 0), Quaternion.identity);
+    }
+}
