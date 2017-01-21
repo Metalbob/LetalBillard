05e2d5c0da4274dc034eda9f170e7104284baf64
Julien Falcioni <dragon9000@hotmail.fr>
2017-01-21 13:16:10 +0100

    add base menu

---
+++ LetalBillard/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject menuContainer;
+    public GameObject waitInputMenuContainer;
+    public GameObject confirmQuitContainer;
+
+    public void Play()
+    {
+        menuContainer.SetActive(false);
+        waitInputMenuContainer.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        menuContainer.SetActive(true);
+        waitInputMenuContainer.SetActive(false);
+    }
+
+    public void ConfirmQuit()
+    {
+        confirmQuitContainer.SetActive(true);
+    }
+
+    public void CloseConfirmMenu()
+    {
+        confirmQuitContainer.SetActive(false);
+    }
+
+	public void Quit()
+    {
+        Application.Quit();
+    }
+}
